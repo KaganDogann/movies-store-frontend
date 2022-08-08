ac82b635242fb3a4a1c3d8f3895a4d596a33cb2c
KaganDogan <kagandoganx@gmail.com>
2022-08-08 10:45:27 +0300

    added custom cards and custom button components and also added slider for movies page

--- src/components/Cards/MovieCard/types.ts
+++ src/components/Cards/MovieCard/types.ts
@@ -1,10 +1,11 @@
 export interface MovieCardProps {
     movieName: string;
     imageSource?: string;
-    description: string;
+    description?: string;
     releaseDate?: Date;
     iMDbRaiting?: number;
-    id: number;
+    id?: number;
     onDelete?: () => void;
+    children: React.ReactNode;
   }
   
--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.scss';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.scss";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<App />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
